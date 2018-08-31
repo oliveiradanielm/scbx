cfff006c02da91334073ed8f74bef3ed943681c7
Jorge Vitório Tavares <norbdus@gmail.com>
2018-08-30 21:10:33 -0300

    adicionando o devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,18 @@ Rails.application.configure do
     Bullet.console = true
     Bullet.rails_logger = true
   end
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "scbxsistema@gmail.com",
+      password: "Scbx@1234()",
+      :openssl_verify_mode  => 'none'
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_052122) do
+ActiveRecord::Schema.define(version: 2018_08_30_223236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -174,13 +174,6 @@ ActiveRecord::Schema.define(version: 2018_08_29_052122) do
     t.index ["etapa_id"], name: "index_inscricoes_on_etapa_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "message"
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tipo_baterias", force: :cascade do |t|
     t.string "descricao"
     t.datetime "created_at", null: false
@@ -197,6 +190,18 @@ ActiveRecord::Schema.define(version: 2018_08_29_052122) do
     t.string "descricao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "useres", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_useres_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_useres_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bateria_detalhes", "baterias"
